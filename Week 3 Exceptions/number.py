724f9039826acfcdec9b4f869ab8dd3af4185f39
Lotus010 <pankaj.sen87@gmail.com>
2022-08-20 16:41:07 +0530

    Update number.py

--- Week 3 Exceptions/number.py
+++ Week 3 Exceptions/number.py
@@ -9,14 +9,14 @@ Created on Sat Aug 20 14:32:38 2022
 #ValueError - Entered value is not type int
 
 def main():
-    x = get_int()
+    x = get_int("What's x? ")
     print(f"x is {x}")
     
-def get_int():
+def get_int(prompt):
     while True:
         try:
             #integers = int(input("What's x? "))
-            return int(input("What's x? "))         
+            return int(input(prompt))         
         except ValueError:
             print("X is not a number")
         #else:
