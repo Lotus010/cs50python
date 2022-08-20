54fe28faa6d266449f82db606fd6524cadc1ba1f
Lotus010 <pankaj.sen87@gmail.com>
2022-08-20 16:33:04 +0530

    Update number.py

--- Week 3 Exceptions/number.py
+++ Week 3 Exceptions/number.py
@@ -5,13 +5,27 @@ Created on Sat Aug 20 14:32:38 2022
 @author: psen
 """
 
+#Handling Exception
+#ValueError - Entered value is not type int
 
+def main():
+    x = get_int()
+    print(f"x is {x}")
+    
+def get_int():
+    while True:
+        try:
+            #integers = int(input("What's x? "))
+            return int(input("What's x? "))         
+        except ValueError:
+            print("X is not a number")
+        #else:
+        #    return integers
+        
 
-try:
-    integers = int(input("What's x? "))    
-except ValueError:
-    print("X is not a number")
-else:
-    print(f"x is {integers}")
+main()
         
-        
+
+    
+#NameError - When int raises exception, x does not gets initialized  
+#print(f"x is {integers}")        
