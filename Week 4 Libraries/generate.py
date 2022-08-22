0d54c3ab6667c335f01687a8928604789f7be44f
Lotus010 <pankaj.sen87@gmail.com>
2022-08-22 14:03:38 +0530

    Create generate.py

---
+++ Week 4 Libraries/generate.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 22 13:41:12 2022
+
+@author: psen
+"""
+'''
+import random 
+
+coin = random.choice(["head","tail"])
+'''
+from random import choice
+
+coin = choice(["head","tail"])
+
+print(coin)
