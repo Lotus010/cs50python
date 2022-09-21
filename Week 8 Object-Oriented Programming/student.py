f3172865361e6f92a5dc14c1a3f0e3063a1d7294
Lotus010 <pankaj.sen87@gmail.com>
2022-09-21 12:30:38 +0530

    Update student.py

--- Week 8 Object-Oriented Programming/student.py
+++ Week 8 Object-Oriented Programming/student.py
@@ -6,22 +6,59 @@ Created on Mon Sep 19 15:38:39 2022
 """
 
 class Student:
-    def __init__(self,name,house):
-        self.n = name
-        self.h = house
-    ...
+    def __init__(self,name,house,patronus):      
+        self.name = name
+        self.house = house
+        self.patronus = patronus
+    
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+    
+    def charm(self):
+        match self.patronus:  #NOQA
+            case "Stag":
+                return "🦄"
+            case _:
+                return "🪄"
+    #Getter
+    @property
+    def name(self):
+        return self._name
+    
+    #Setter
+    @name.setter
+    def name(self,name):
+        if not name:
+            raise ValueError("Missing Name")
+        self._name = name    
+
+            
+    #Getter
+    @property
+    def house(self):
+        return self._house
+
+    #Setter
+    @house.setter
+    def house(self, house):
+        if house not in ["Gryffindor", "Slytherin"]:
+            raise ValueError("Invalid House")
+        self._house = house       
     
 
 def main():
     S = get_student()
-    print(f"{S.name} from {S.house}")
+    print(S)
+    print("Expecto Patronum")
+    print(S.charm())
+
 
 
 def get_student():
     n = "Harry"
-    h = "Gryffindor"      
-    student = Student(n,h)
-    return student
+    h = "Gryffindor"
+    p = "Stag"      
+    return Student(n,h,p)
 
 
 if __name__  ==  "__main__":
